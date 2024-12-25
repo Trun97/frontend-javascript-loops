0b671c35fb0b611dd62ada771a4caf50f7f21b0d
Trun97 <tarun7283@gmail.com>
2024-12-25 16:12:44 +0100

    huiswerk

--- loops.js
+++ loops.js
@@ -6,11 +6,13 @@
 // ***
 // ****
 // *****
+// for (let i = 0; i <=5 ; i++) {
+//     console.log("*".repeat(i));
+// }
 
 // Tip: je kunt de .repeat() methode gebruiken om een karakter een n aantal keer te herhalen...
 // Dit heb je nog niet geleerd, maar bekijk hiervoor dit MDN-artikel eens: https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/String/repeat
 // ==========================================
-
 
 
 
@@ -25,7 +27,13 @@
 // klaar!
 // ==========================================
 
-
+// const loop = "loop...";
+// const done = "klaar!";
+// for (let i = 0; i <=4 ; i++) {
+//     if (i < 4) {
+//         console.log(loop);
+//     } else console.log(done);
+// }
 
 
 
@@ -44,6 +52,16 @@
 // >> 8
 // >> 9
 // ==========================================
+
+for (let i = 0; i < 10; i++) {
+    console.log(i);
+    if (i > 2) {
+        console.log(">" + i)
+    } else if (i > 6) {
+        console.log(">>" + i)
+    }
+}
+// FOUT DEZE LUKT NIET, GEEN IDEE
 
 
 
